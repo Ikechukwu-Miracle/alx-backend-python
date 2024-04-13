c2fbf9f7db0e5d37f0961944f963cf537d3df442
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-04-13 03:51:41 +0100

    initial commit including all essential files

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,8 @@ from utils import access_nested_map
 
 class TestAcessNestedMap(Unittest.Testcase):
     """Class for testing the nested map"""
+    # unittest does not support test decorators,
+    # only tests created with @parameterized.expand will be executed
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -14,7 +16,8 @@ class TestAcessNestedMap(Unittest.Testcase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
 
-    def test_access_nested_map(self, nested_map, path, expected_result):
+    def test_access_nested_map(self, map, path, expected_result):
         """Test method that for expected results"""
 
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+        actual_output = access_nested_map(map, path)
+        self.assertEqual(actual_output, expected_output)
